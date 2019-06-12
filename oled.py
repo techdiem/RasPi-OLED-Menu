72612472e33e552b19e2d962fa3d5918bc02a97c
techdiem <48885218+techdiem@users.noreply.github.com>
2019-06-12 18:41:31 +0200

    Added settings file and connection to Mopidy via MPD

--- oled.py
+++ oled.py
@@ -1,28 +1,37 @@
+from time import sleep
 from luma.core.interface.serial import i2c
 from luma.core.render import canvas
 from luma.oled.device import sh1106
 from RPi import GPIO
-from time import sleep
+import musicpd
+import configparser
+
+#Load Config
+config = configparser.ConfigParser()
+config.read("settings.ini")
 
 #Setup pins for rotary encoder
-clk = 17
-dt = 18
-sw = 27
+clk = int(config.get('Pins', 'clk'))
+dt = int(config.get('Pins', 'dt'))
+sw = int(config.get('Pins', 'sw'))
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(sw, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-# GPIO.add_event_detect(clk, GPIO.FALLING  , callback=my_callback, bouncetime=130)
-# GPIO.add_event_detect(sw, GPIO.FALLING  , callback=my_callback, bouncetime=300)
+# GPIO.add_event_detect(clk, GPIO.FALLING, callback=my_callback, bouncetime=130)
+# GPIO.add_event_detect(sw, GPIO.FALLING, callback=my_callback, bouncetime=300)
 
 #Setup OLED display
 device = sh1106(i2c(port=1, address=0x3C))
 
+#Setup Connection to Mopidy
+client = musicpd.MPDClient()
+client.connect(config.get('MPD', 'ip'), int(config.get('MPD', 'port')))
+
 with canvas(device) as draw:
-    draw.rectangle(device.bounding_box, outline="white", fill="black")
     draw.text((30, 40), "Hello World", fill="white")
 
 while True:
-    sleep(1)
+    sleep(2)
 
 GPIO.cleanup()
