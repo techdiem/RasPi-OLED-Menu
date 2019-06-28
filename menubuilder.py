aa3cdaed7353ae0d07d9ae01afd3d6bb9c7d0e79
techdiem <48885218+techdiem@users.noreply.github.com>
2019-06-28 14:10:49 +0200

    added some font-awesome icons so that the IDLE screen looks better

--- menubuilder.py
+++ menubuilder.py
@@ -8,15 +8,15 @@ from luma.core.render import canvas
 from setupHandler import client
 today_last_time = "Unknown"
 
-def posn(angle, arm_length):
-    dx = int(math.cos(math.radians(angle)) * arm_length)
-    dy = int(math.sin(math.radians(angle)) * arm_length)
-    return (dx, dy)
-
 def menuentry(draw, x, y, text):
+    fontawesome = ImageFont.truetype("fontawesome.ttf", size=10)
     font = ImageFont.truetype("bahnschrift.ttf", size=10)
     draw.rectangle((x, y, x+120, y+12), outline=255, fill=0)
-    draw.text((x+2, y), text, font=font, fill="white")
+    if text == "Zurück":
+        draw.text((x+2, y+1), text="\uf053", font=fontawesome, fill="white")
+        draw.text((x+12, y), text, font=font, fill="white")
+    else:
+        draw.text((x+2, y), text, font=font, fill="white")
 
 def drawmenu(draw, entries):
     position = 0
@@ -28,6 +28,7 @@ def drawIdle(device):
     global today_last_time
     clockfont = ImageFont.truetype("kristenITC.ttf", size=35)
     font = ImageFont.truetype("calibri.ttf", size=12)
+    fontawesome = ImageFont.truetype("fontawesome.ttf", size=12)
     now = datetime.datetime.now()
     today_time = now.strftime("%H:%M")
     if today_time != today_last_time:
@@ -35,11 +36,12 @@ def drawIdle(device):
         with canvas(device) as draw:
             now = datetime.datetime.now()
 
-            draw.text((20, 5), today_time, font=clockfont, fill="white")
+            draw.text((20, 3), today_time, font=clockfont, fill="white")
             playingInfo = client.currentsong()
             if playingInfo != {}:
                 currentSong = playingInfo["title"]
-                draw.text((3, 50), currentSong[0:21], font=font, fill="white")
+                draw.text((12, 48), currentSong[0:21], font=font, fill="white")
+                draw.text((0, 45), text="\uf001", font=fontawesome, fill="white")
 
 def menuUsed(draw, entries):
     counter = initGlobals.counter
