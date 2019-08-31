e05f1b09debe47ff9a57e786157da71c635bf307
techdiem <48885218+techdiem@users.noreply.github.com>
2019-08-31 22:12:39 +0200

    now the rotary encoder works great in both directions

--- setuphandler.py
+++ setuphandler.py
@@ -4,15 +4,16 @@ try:
 except: pass
 import configparser
 import musicpd
+import threading
 from luma.oled.device import sh1106
 from luma.core.interface.serial import i2c
 import helperFunctions
 
 print("Starting OLED display on werkstattpi:")
-client = musicpd.MPDClient()
+print()
 
 #Setup OLED display
-print("Connect to display...")
+print("Connect to display")
 device = sh1106(i2c(port=1, address=0x3C))
 device.contrast(245)
 
@@ -31,10 +32,14 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(sw, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-clkLastState = GPIO.input(clk)
+current_clk = 1
+current_dt = 1
+LockRotary = threading.Lock()   # create lock for rotary switch
 
 #Setup Connection to Mopidy
 print("Connect to Mopidy")
+client = musicpd.MPDClient()
+
 try:
     client.connect(config.get('MPD', 'ip'), int(config.get('MPD', 'port')))
     client.clear()
@@ -46,26 +51,38 @@ except:
     device.cleanup()
     exit()
 
-#Interrupt handler for re
+#Interrupt handler for push button in rotary encoder
 def menuaction(channel):
     helperFunctions.trigger = True
 
+#Interrupt handler for turning the rotary encoder (changing the counter)
 def rotary_detect(channel):  
-    global clkLastState
-    try:
-        clkState = GPIO.input(clk)
-        if clkState != clkLastState:
-            dtState = GPIO.input(dt)
-            if dtState != clkState:
+    global current_clk, current_dt, LockRotary
+    Switch_A = GPIO.input(clk)
+    Switch_B = GPIO.input(dt)
+    
+    if current_clk == Switch_A and current_dt == Switch_B:
+        #Same interrupt as before? -> Bouncing -> no actio
+        pass
+    else:
+        #refresh saved state
+        current_clk = Switch_A
+        current_dt = Switch_B
+        
+        if (Switch_A and Switch_B): #both ones active
+            LockRotary.acquire()
+            if channel == dt: #Direction depends on which one was last
                 helperFunctions.counter += 1
             else:
-                helperFunctions.counter -= 1
-                clkLastState = clkState
-    except:
-        print("rotary encoder error")
+                helperFunctions.counter -= 1 #Set Counter
+            LockRotary.release()
 
-print("Adding interrupts for rotary encoder")
-GPIO.add_event_detect(clk, GPIO.FALLING, callback=rotary_detect, bouncetime=150)
+
+print("Attaching Interrupts")
+#Rotary encoder
+GPIO.add_event_detect(clk, GPIO.RISING, callback=rotary_detect)
+GPIO.add_event_detect(dt, GPIO.RISING, callback=rotary_detect)
+#Push Button
 GPIO.add_event_detect(sw, GPIO.FALLING, callback=menuaction, bouncetime=300)
 
 print("Setup ready!")
